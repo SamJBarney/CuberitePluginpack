26c11889750dea7797a2e97a5d9c01777b48ee03
Samuel Barney <samjbarney@gmail.com>
2015-07-01 15:49:21 -0600

    Fixed version number
    Version numbers are not allowed to be a string.

--- AutoReplant/Info.lua
+++ AutoReplant/Info.lua
@@ -1,7 +1,7 @@
 g_PluginInfo =
 {
 	Name = "Auto Replant",
-	Version = "0.1 Beta",
+	Version = 0.1,
 	Date = "2015-06-25",
 	Description = "This plugin makes right-clicking a fully-grown crop harvest and automatically replant it.",
 	
