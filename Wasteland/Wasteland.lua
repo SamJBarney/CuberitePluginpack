1a09f104b6c459a4e7bdd2a6a825b94766e444aa
Samuel Barney <samjbarney@gmail.com>
2015-07-06 17:01:05 -0600

    Breaking Dead Bush callback
    Set up a simple system where a callback for a block type can be
    assigned.
    
    Dead Bushes now drop sticks and an occasional ‘Unknown Seed’.

--- Wasteland/Wasteland.lua
+++ Wasteland/Wasteland.lua
@@ -22,6 +22,9 @@ function Initialize(Plugin)
 	-- Crafting Hooks
 	cPluginManager.AddHook(cPluginManager.HOOK_PRE_CRAFTING, OnPreCrafting)
 
+	-- Misc Hooks
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_BROKEN_BLOCK, OnBlockBroken)
+
 	LOG("Initialized " .. PLUGIN:GetName() .. " v." .. PLUGIN:GetVersion())
 
 	return true
@@ -87,3 +90,47 @@ function OnPreCrafting(Player, Grid, Recipe)
 
 	return recipe_found
 end
+
+local BrokenBlockHooks = {}
+
+
+
+-- Block Breaking Callback
+function OnBlockBroken(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
+	local handler = BrokenBlockHooks[BlockType]
+	if handler ~= nil then
+		return handler(Player, BlockX, BlockY, BlockZ, BlockFace, BlockMeta)
+	end
+end
+
+function OnDeadBushBroken(Player, BlockX, BlockY, BlockZ, BlockFace, BlockMeta)
+
+	-- Only call this handler if the bush was broken by a player
+	if Player == nil then
+		return false
+	end
+
+
+	local equipped = Player:GetEquippedItem()
+
+	-- Only drop things if the player is not equipping shears
+	if equipped.m_ItemType ~= E_ITEM_SHEARS then
+		Pickups = cItems()
+
+		-- Give 1 to 2 sticks
+		Pickups:Add(cItem(E_ITEM_STICK, math.random(1,2)))
+
+		-- Only spawn seeds every once in a while
+		if math.random() < 0.5 then
+			Pickups:Add(w_Items['wasteland:seed']:CopyOne())
+		end
+
+		-- Spawn the item drops
+		local World = Player:GetWorld()
+		World:SpawnItemPickups(Pickups, BlockX,BlockY,BlockZ, 0)
+	return true
+	end
+	return false
+end
+
+BrokenBlockHooks[E_BLOCK_DEAD_BUSH] = OnDeadBushBroken
