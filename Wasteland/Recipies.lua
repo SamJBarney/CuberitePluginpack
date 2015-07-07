32f837b6b0ac78bea5351c57885ca9866925ef1f
Samuel Barney <samjbarney@gmail.com>
2015-07-07 12:36:42 -0600

    Added Sunbleached Wood recipe

---
+++ Wasteland/Recipies.lua
@@ -0,0 +1,12 @@
+wasteland_Recipies = {}
+
+local stick = cItem(E_ITEM_STICK)
+
+local tmp = CraftingRecipe.new(2,2)
+tmp:SetResult(w_Items['wasteland:dry_planks'])
+tmp:SetItem(0,0, stick)
+tmp:SetItem(0,1, stick)
+tmp:SetItem(1,0, stick)
+tmp:SetItem(1,1, stick)
+
+table.insert(wasteland_Recipies, tmp)
